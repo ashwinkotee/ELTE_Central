9dde6731b1d81e1ba90bbfd507c74be5fb84f5fd
ashwinkotee <ashwinkumar7496@gmail.com>
2020-12-08 12:53:25 +0530

    new files

---
+++ js/buttonScript.js
@@ -0,0 +1,6 @@
+<script>
+  function Teams() {
+    window.open("https://www.microsoft.com/en-in/microsoft-365/microsoft-teams/group-chat-software");
+    window.open("http://www.microsoft.com/en-in/microsoft-365/microsoft-teams/group-chat-software");
+  }
+</script>
